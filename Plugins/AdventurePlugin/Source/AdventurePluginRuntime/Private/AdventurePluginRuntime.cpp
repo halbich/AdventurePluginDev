ad18e5dcc901414ed8af469cb0ecc31781eb6bbb
Pavel Halbich <halbich.p@hotmail.com>
2017-11-23 20:55:30 +0100

    initial commit

---
+++ Plugins/AdventurePlugin/Source/AdventurePluginRuntime/Private/AdventurePluginRuntime.cpp
@@ -0,0 +1,32 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "CoreMinimal.h"
+#include "Modules/ModuleManager.h"
+#include "IAdventurePluginRuntime.h"
+
+
+class FAdventurePluginRuntime : public IAdventurePluginRuntime
+{
+	/** IModuleInterface implementation */
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+};
+
+IMPLEMENT_MODULE( FAdventurePluginRuntime, AdventurePluginRuntime )
+
+
+
+void FAdventurePluginRuntime::StartupModule()
+{
+	// This code will execute after your module is loaded into memory (but after global variables are initialized, of course.)
+}
+
+
+void FAdventurePluginRuntime::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+
+
