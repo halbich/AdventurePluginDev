622886ce453e72644fe39e5fe5a538c51e69bf79
Pavel Halbich <halbich@users.noreply.github.com>
2018-05-17 22:37:14 +0200

    testy

---
+++ Plugins/AdventurePlugin/Source/AdventurePluginRuntime/GameStateMonitor/AdventurePluginGameInstance.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "AdventurePluginGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ADVENTUREPLUGINRUNTIME_API UAdventurePluginGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
